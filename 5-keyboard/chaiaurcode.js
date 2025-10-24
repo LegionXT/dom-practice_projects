add9ab8eb06c01b66f2313b3c6e524e8a0097579
Akash Rawat <virat0996@gmail.com>
2025-10-25 03:40:51 +0530

    Implement keydown event listener for key info display
    Add event listener to display key information in a table.

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,27 @@
-console.log('Project 5');
+//First taking the reference of key that presses.
+const insert = document.getElementById('insert')
+
+//Now apply eventListner on whole window ---
+window.addEventListener('keydown',(e) => {
+  insert.innerHTML = `
+   <div class = 'color'>
+                       //  Now we bring a table here ....
+  
+  <table>
+  <tr>
+    <th>key</th>
+    <th>keyCode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+                         // here, we extract from the event ...
+    
+    <td>${e.key === ' ' ? 'Space' : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.Code}</td>
+  </tr>
+</table>
+   </div>
+  `;
+}); 
+
